810097adfbc49542aa862c962fe27d189aefc046
Ethan Brandon Bates <batese14@gmail.com>
2018-07-22 21:07:43 +0000

    Added JS Messages features

--- application.py
+++ application.py
@@ -8,40 +8,32 @@ app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
 # list of all channels
-channel_list = ['one','two','three']
-user_messages = {}
+channel_list = ['general']
+user_messages = []
 
 # Render Index Template
 @app.route("/", methods=["GET", "POST"])
 def index():
     return render_template("index.html", channels=channel_list)
 
+
 #Adding Channels
 @socketio.on('add_new_channel')
 def add_new_channel(chjson):
     new_channel= chjson['name']
     if new_channel in channel_list:
-        emit('channel_alert',{'data': 'Channel Already Exists!'})
+        emit('channel_alert',{'data': f"Channel '{new_channel}' Already Exists!" })
     else:
         channel_list.append(new_channel)
         emit('display_channel',new_channel, broadcast=True)
 
-#User Login and Display Name
 
+#Message Room Control
+@socketio.on('user_messages')
+def handle_usr_message(message):
+    print(message)
+    emit('msg_response', {'msg_user': message['user'], 'msg_message': message['msgcontent'], 'msg_time': message['timestamp'] }, broadcast=True)
 
-
-
-#Room Control
-@socketio.on('my_room_event')
-def send_room_message(message):
-    emit('my_response',{'data': message['data']} ,room=message['room'])
-
-
-#Messaging
-@socketio.on('message')
-def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/index.js
+++ static/index.js
@@ -1,30 +1,83 @@
   // Web Socket Set Up
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+  var socket_info = {
+    'session_id': '',
+    'display_name': '',
+    'room_name': ''
+  }
 
   // Connect to websocket
   socket.on('connect', function() {
     console.log('Websocket connected!');
-    console.log(socket.id);
+    socket_info['session_id'] = socket.id ;
   });
 
+  // Timestamp Format
+  function timestamp_format() {
+    var myDate = new Date();
+	  var dateStr = myDate.getMonth()+1 + '/' + myDate.getDate() + '/' + myDate.getFullYear();
+	  var tsHours = myDate.getHours();
+	  if (tsHours > 12 ) {
+		  tsStamp = (tsHours - 12) + ':' + myDate.getMinutes() + ' ' + 'PM';
+	  } else {
+		  tsStamp = tsHours + ':' + myDate.getMinutes() + ' ' + 'AM' ;
+	  };
+	  strStamp = dateStr + ' ' + tsStamp;
+	  return strStamp;
+}
+
 document.addEventListener('DOMContentLoaded', function() {
+
+  //Display Name
+  document.getElementById('choosename').onsubmit = function() {
+    display_name = document.getElementById('usernameInput').value;
+    socket_info['display_name'] = display_name;
+    console.log(socket_info);
+
+    var userWelcome = document.getElementById('welcome_user').innerHTML
+    userWelcome = 'Welcome ' + display_name;
+    document.getElementById('welcome_user').innerHTML = userWelcome;
+
+    //Hide User Input Form and Set Message Center to Enabled
+    document.getElementById('choosename').style.display = 'none';
+    document.getElementById('myMessage').disabled = false;
+
+    return false;
+  };
+
 
   // Add Channels
   document.querySelector('#addchannel').onsubmit = function() {
     const chName = document.forms["addchannel"]["channel"].value;
     console.log(chName);
     socket.emit('add_new_channel', {'name': chName});
+    document.forms["addchannel"]["channel"].value = '';
     return false
   };
 
   // Alert if Channel Already Exists
   socket.on('channel_alert', function(msg) {
     console.log(msg);
+
     var chAlert = document.createElement('div');
     chAlert.innerHTML = msg['data'];
     chAlert.setAttribute("role","alert");
     chAlert.setAttribute("class","alert alert-danger alert-dismissible");
+
+    var closeBtn = document.createElement('button');
+    closeBtn.setAttribute("type","button");
+    closeBtn.setAttribute("class","close");
+    closeBtn.setAttribute("data-dismiss","alert");
+    closeBtn.setAttribute("aria-label","Close");
+
+    var closeX = document.createElement('span');
+    closeX.setAttribute("aria-hidden","true");
+    closeX.innerHTML = 'X';
+
     document.querySelector('#addchannel').appendChild(chAlert);
+    chAlert.appendChild(closeBtn);
+    closeBtn.appendChild(closeX);
+
     return false
   });
 
@@ -42,29 +95,41 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-  // Messages
+  // Message Send
   document.querySelector('#sendbutton').onclick = function() {
     var msgcontent = document.querySelector('#myMessage').value;
+    var timestamp = timestamp_format()
     console.log(msgcontent);
-    socket.send(msgcontent);
+    socket.emit('user_messages',
+        {'user': socket_info['display_name'], 'msgcontent': msgcontent, 'timestamp': timestamp, 'room': 'general' });
+    return false;
   };
 
-  socket.on('message', (msg) => {
+  // Message Listen
+  socket.on('msg_response', (msg) => {
     var newMessage = document.createElement('li');
-    newMessage.innerHTML = msg;
+    var userWrap = document.createElement('span');
+    var userTimeStmp = document.createElement('span');
+    var userMsg = document.createElement('p');
+
+    //UserName
+    userWrap.innerHTML = msg['msg_user'] + ': ';
+    userWrap.setAttribute("class","msg_username");
+
+    //Timestamp
+    userTimeStmp.innerHTML = '[' + msg['msg_time'] + ']';
+    userTimeStmp.setAttribute("class","msg_timestamp");
+
+    //Message Contents
+    userMsg.innerHTML = msg['msg_message'];
+    userMsg.setAttribute("class","msg_user_content");
+
+    newMessage.appendChild(userWrap);
+    newMessage.appendChild(userTimeStmp);
+    newMessage.appendChild(userMsg);
+
     document.querySelector("#messages").appendChild(newMessage);
-    console.log("Received Message")
+    console.log("Received " + socket_info['display_name'] + msg);
   });
 
 });
